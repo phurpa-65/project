6859a78838a52e668332bb2cab773110c4a62b08
phurpa <phurpalhamo579@gmail.com>
2022-09-22 11:27:15 +0600

    set viewport width

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     viewportHeight:1080,
-    viewportHeight:1080,
+    viewportWidth:1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
